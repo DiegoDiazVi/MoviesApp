f859c028505aaa2d055c5c09fdc142eaff3e04e9
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2022-05-19 18:36:07 -0500

    add api consumption for categories

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ const getTrendingMoviesPreview = async () =>{
         const movies = data.results;
         const movieImgArticle = document.querySelector('#trendingPreview .container-carrousel');
         const fragment = new DocumentFragment();
-        movies.forEach(movie => {
+        movies.map(movie => {
             const movieContainer = document.createElement('figure');
             movieContainer.classList.add('container-img');
-    
             const movieImg = document.createElement('img');
             movieImg.classList.add('images');
             movieImg.setAttribute('alt', movie.title);
@@ -25,4 +24,34 @@ const getTrendingMoviesPreview = async () =>{
     }
 }
 
-getTrendingMoviesPreview()
+const getCategoriesPreview = async () => {
+    try {
+        const response = await fetch (`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+        const data = await response.json();
+        console.log(data);
+        const genres = data.genres;
+        const fragment = new DocumentFragment();
+        const genreArticle = document.querySelector('#categoriesPreview .container-subcategories');
+        genres.map(genre =>{
+            const containerGenre = document.createElement('div');
+            containerGenre.classList.add('container-subcategories--name');
+            const colorGenre = document.createElement('div');
+            colorGenre.classList.add('color');
+            colorGenre.setAttribute('id', 'id'+genre.id)
+            const genreTitle = document.createElement('h3');
+            genreTitle.classList.add('name-categories');
+            const genreTitleText = document.createTextNode(genre.name);
+
+            genreTitle.appendChild(genreTitleText);
+            containerGenre.append(colorGenre,genreTitle);
+            fragment.appendChild(containerGenre);
+        })
+        genreArticle.appendChild(fragment);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
