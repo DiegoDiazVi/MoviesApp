068fa1a51bcec16fbac788b1de2e73fbec5047bc
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2022-05-19 17:38:18 -0500

    add api consumption for image movies

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
 import { API_KEY } from "./secrets.js";
 
-console.log(API_KEY);
+const getTrendingMoviesPreview = async () =>{
+    try {
+        const response = await fetch (`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+        const data = await response.json();
+        const movies = data.results;
+        const movieImgArticle = document.querySelector('#trendingPreview .container-carrousel');
+        const fragment = new DocumentFragment();
+        movies.forEach(movie => {
+            const movieContainer = document.createElement('figure');
+            movieContainer.classList.add('container-img');
+    
+            const movieImg = document.createElement('img');
+            movieImg.classList.add('images');
+            movieImg.setAttribute('alt', movie.title);
+            movieImg.setAttribute('src','https://www.themoviedb.org/t/p/w300'+ movie.poster_path);
+    
+            movieContainer.appendChild(movieImg);
+            fragment.appendChild(movieContainer);
+        });
+        movieImgArticle.appendChild(fragment);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getTrendingMoviesPreview()
